fb1ad81f267f4a51b77c357727b91b2330c2529b
Valentin <valentindabovski@gmail.com>
2020-06-15 21:41:10 +0200

    add two more api projects, customers and identity

--- PizzaShop.Customers/Startup.cs
+++ PizzaShop.Customers/Startup.cs
@@ -4,8 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
-namespace PizzaShop
+namespace PizzaShop.Customers
 {
     public class Startup
     {
